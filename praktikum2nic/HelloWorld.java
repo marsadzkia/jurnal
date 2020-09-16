cfcfedcc701f74be5d316e1d063f478d898b0cda
marsadzkia <marsaadz06@gmail.com>
2020-09-16 11:23:18 +0700

    HelloWorld.java

---
+++ praktikum2nic/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Nama: Marsa");
+		System.out.println("NIM: 6706202107");
+		System.out.println("Kelas: D3IF4402");
+	}
+}
